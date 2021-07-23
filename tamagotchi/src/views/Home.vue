030cbb8d80d3542b08b74c97bbf15c6f4fc52bfd
Ottavio Fogliata <5075650+ott-fogliata@users.noreply.github.com>
2021-07-23 12:12:53 +0200

    router in vue.js

---
+++ tamagotchi/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <input v-model="tamaName" />
+    <!--<a :href="'/tama/' + tamaName">Crea Tamagotchi</a>-->
+    <router-link :to="'/tama/' + tamaName">Crea Tamagotchi</router-link>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  data() {
+    return {
+      tamaName: ''
+    }
+  }
+}
+</script>
